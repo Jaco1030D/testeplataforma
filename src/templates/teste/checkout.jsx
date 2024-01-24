0aaacc3de41dda10048f81cd0db600630c9b7224
Jaco Antonio Magalhães <jaco42818@gmail.com>
2024-01-24 15:20:17 -0300

    subindo

--- src/templates/teste/checkout.jsx
+++ src/templates/teste/checkout.jsx
@@ -58,7 +58,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:8888/order",
+        return_url: "https://glowing-profiterole-535857.netlify.app/order",
       },
     });
 
