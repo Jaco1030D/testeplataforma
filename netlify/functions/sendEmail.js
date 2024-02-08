43be8f2c363e11e1f26cf6ba054921e2a4830b61
Jaco Antonio Magalhães <jaco42818@gmail.com>
2024-02-08 09:01:04 -0300

    subindo

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -209,7 +209,8 @@ exports.handler = async (event) => {
       console.log(error);
 
       return {
-        statusCode: 500
+        statusCode: 500,
+        body: JSON.stringify({error})
       }
     
     }
