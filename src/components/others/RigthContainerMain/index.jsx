f6c09cd9b5798777cbf8796ac05a92ae90e3a7d4
Jaco Antonio Magalhães <jaco42818@gmail.com>
2024-02-14 21:07:43 -0300

    subindo

--- src/components/others/RigthContainerMain/index.jsx
+++ src/components/others/RigthContainerMain/index.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
-// import img from './image.svg'
+import img from './image.svg'
 
 const RigthContainerMain = () => {
   return (
     <div className='main_rigth'>
-      {/* <img src={img} alt="" /> */}
+      <img src={img} alt="" />
     </div>
   )
 }
